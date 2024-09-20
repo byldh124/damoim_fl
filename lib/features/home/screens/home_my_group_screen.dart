46df5a6d4b46cc1acacc93d6662b37b033b5b48b
byldh124 <byldh124@gmail.com>
2024-09-20 14:17:23 +0900

    search, mygroup

--- lib/features/home/screens/home_my_group_screen.dart
+++ lib/features/home/screens/home_my_group_screen.dart
@@ -1,3 +1,7 @@
+import 'package:damoim/core/const/data.dart';
+import 'package:damoim/features/home/provider/group_list_provider.dart';
+import 'package:damoim/features/home/provider/state/group_list_ui_state.dart';
+import 'package:damoim/features/home/widget/group_card.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -9,10 +13,32 @@ class HomeMyGroupScreen extends ConsumerStatefulWidget {
 }
 
 class _HomeMyGroupScreenState extends ConsumerState<HomeMyGroupScreen> {
+
+  @override
+  void initState() {
+    super.initState();
+    ref.read(groupListProvider.notifier).getList(id: 'test01', type: GroupType.MYGROUP);
+  }
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('my group'),
+    final state = ref.watch(groupListProvider);
+    return Center(
+      child: switch (state) {
+        Success() => ListView.builder(
+          shrinkWrap: false,
+          itemCount: state.list.length,
+          itemBuilder: (context, index) {
+            final item = state.list[index];
+            return GroupCard(model: item);
+          },
+        ),
+        Loading() => const Center(
+          child: CircularProgressIndicator(),
+        ),
+        Error() => Center(
+          child: Text(state.message),
+        ),
+      },
     );
   }
 }
--- lib/features/home/screens/home_search_screen.dart
+++ lib/features/home/screens/home_search_screen.dart
@@ -1,5 +1,8 @@
 import 'package:damoim/application/config/component/custom_text_form_field.dart';
+import 'package:damoim/core/domain/model/entity/group_item_model.dart';
 import 'package:damoim/features/home/provider/group_list_provider.dart';
+import 'package:damoim/features/home/provider/state/group_list_ui_state.dart';
+import 'package:damoim/features/home/widget/group_card.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -12,16 +15,43 @@ class HomeSearchScreen extends ConsumerStatefulWidget {
 
 class _HomeSearchScreenState extends ConsumerState<HomeSearchScreen> {
   String query = '';
+
   @override
   Widget build(BuildContext context) {
     final state = ref.watch(groupSearchProvider(query));
-    debugPrint('state : ${state.map((e) => e.title)}');
-    return CustomTextFormField(
-      onChanged: (t) {
-        setState(() {
-          query = t;
-        });
-      },
+    return Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: CustomTextFormField(onChanged: (t) {
+            setState(() {
+              query = t;
+            });
+          }),
+        ),
+        if (state.isEmpty) _emptyMessage(),
+        if (state.isNotEmpty) _buildList(state)
+      ],
+    );
+  }
+
+  Widget _buildList(List<GroupItemModel> state) {
+    return SizedBox(
+      height: 400,
+      child: ListView.builder(
+        shrinkWrap: false,
+        itemCount: state.length,
+        itemBuilder: (context, index) {
+          final item = state[index];
+          return GroupCard(model: item);
+        },
+      ),
+    );
+  }
+
+  Widget _emptyMessage() {
+    return Center(
+      child: Text('검색결과 없음'),
     );
   }
 }
